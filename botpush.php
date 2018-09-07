ddc552bb88fc66b1f7a4475b1eaa0c7afa02717f
cognacp <43064504+cognacp@users.noreply.github.com>
2018-09-07 18:03:45 +0700

    Update botpush.php

--- botpush.php
+++ botpush.php
@@ -7,7 +7,9 @@ require "vendor/autoload.php";
 $access_token = 'SitF5yNqyvmV2Pki4tQaR1ROByP+WsAEBjB8HZxExso59O/FLkbBXbORMe9NQIlWWY2qaddLR9vXriXW8UlHKhfkVvDN5wxKJQYPrGZUtPA77RJ9HjIsAHYj1NHlVXVsQUaQrwWYK/w+h+0/XWcO3gdB04t89/1O/w1cDnyilFU=
 ';
 
-$pushID = '1605981794';
+$channelSecret = '9096d6a4b40970f6f302a597ff4366fe';
+
+$pushID = 'Ufbbf0b16c2578d6c7df2b29034d63bc2';
 $httpClient = new \LINE\LINEBot\HTTPClient\CurlHTTPClient($access_token);
 $bot = new \LINE\LINEBot($httpClient, ['channelSecret' => $channelSecret]);
 $textMessageBuilder = new \LINE\LINEBot\MessageBuilder\TextMessageBuilder('hello world');
