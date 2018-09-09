fe51b4b3b126bd3786fbea1b4f7137cd990c9cd6
cognacp <43064504+cognacp@users.noreply.github.com>
2018-09-10 00:05:46 +0700

    Update Hello.php

--- Hello.php
+++ Hello.php
@@ -1,12 +1,12 @@
 <?php
 echo "Hello World สวัสดีชาวโลก"."<br>";
 define("CONSTANT", "Hello world.");
-echo "define("CONSTANT", "Hello world.")."<br>";"
+echo "define("CONSTANT", "Hello world.")";
 echo CONSTANT."<br>"; // outputs "Hello world."
-echo "Constant."<br>"; // outputs "Constant" and issues a notice."."<br>"
+echo "Constant."<br>"; // outputs "Constant" and issues a notice.";
 echo Constant."<br>"; // outputs "Constant" and issues a notice.
 define("GREETING", "Hello you.", true);
-echo "define("GREETING", "Hello you.", true);"."<br>"
+echo "define("GREETING", "Hello you.", true);";
 echo GREETING."<br>"; // outputs "Hello you."
 echo Greeting."<br>"; // outputs "Hello you."
 ?>
